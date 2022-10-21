94b5a357067e14c99be9b9a221b17f1820b10bab
karim <k.hamdi1995@gmail.com>
2022-10-21 03:35:57 +0200

    part3-ex3.2-phonebookBE

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -28,6 +28,13 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+  const phonebookCount = persons.length
+  const date = new Date()
+
+  response.send(`<p>Phonebook has info for ${phonebookCount} people</p><p>${date}</p>`)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
