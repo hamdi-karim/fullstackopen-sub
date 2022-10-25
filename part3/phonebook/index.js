5327290eb7b109ee213fb0d0cc9c91ff92478675
karim <k.hamdi1995@gmail.com>
2022-10-25 23:58:24 +0200

    extract error handling to its own middleware - part3c - ex3.16

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -77,11 +77,12 @@ app.post('/api/persons', (request, response) => {
   })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Phonebook.findByIdAndRemove(request.params.id)
     .then(result => {
       response.status(204).end()
     })
+    .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
@@ -93,6 +94,24 @@ app.get('/info', (request, response) => {
   <p>${date}</p>
   `)
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
