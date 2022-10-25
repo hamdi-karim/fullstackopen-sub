8d23b1277fa5580b6bfab86a3892bcda18098cdf
karim <k.hamdi1995@gmail.com>
2022-10-25 23:53:45 +0200

    apply delete & reflect to database - part3c - ex3.15

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -78,10 +78,10 @@ app.post('/api/persons', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(p => p.id !== id)
-  
-  response.status(204).end()
+  Phonebook.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
 })
 
 app.get('/info', (request, response) => {
