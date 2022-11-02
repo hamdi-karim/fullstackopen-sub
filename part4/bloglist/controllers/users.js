653dd0dfa9dbcc41a9dc22069dc00b2670ca1cda
karim <k.hamdi1995@gmail.com>
2022-11-02 18:59:07 +0100

    Add restrictions when calling POST user - username/pwd

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -10,6 +10,26 @@ usersRouter.get('/', async (request, response) => {
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
 
+  const existingUser = await User.findOne({ username })
+  if (existingUser) {
+    return response.status(400).json({
+      error: 'username must be unique'
+    })
+  }
+
+  if (!password) {
+    return response.status(400).json({
+      error: 'User validation failed: password: Path `password` is required'
+    })
+  }
+
+  if (password.length < 3) {
+    return response.status(400).json({
+      error: 'password must contain a minimum of 3 charchters'
+    })
+  }
+
+
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(password, saltRounds)
 
