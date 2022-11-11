e606a9ec1a4199a58da8839b2404b9e188472eb9
Karim Hamdi <k.hamdi1995@gmail.com>
2022-11-11 18:35:46 +0100

    save user info to localStorage and implement logout - part 5 ex5.2 step 2

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -16,6 +16,15 @@ const App = () => {
       setBlogs( blogs )
     )  
   }, [])
+
+  useEffect(() => {
+    const loggedInUserJSON = window.localStorage.getItem('loggedInUser')
+    if (loggedInUserJSON) {
+      const user = JSON.parse(loggedInUserJSON)
+      setUser(user)
+    }
+  }, [])
+  
   
   const handleLogin = async (event) => {
     event.preventDefault()
@@ -24,13 +33,23 @@ const App = () => {
       const user = await loginService.login({
         username, password
       })
+      window.localStorage.setItem(
+        'loggedInUser', JSON.stringify(user)
+      )
       setUser(user)
       setUsername('')
       setPassword('')
     } catch (error) {
       alert('Wrong Credentials')
+      console.error(error)
     }
   } 
+
+  const handleUserLogout = () => {
+    window.localStorage.removeItem('loggedInUser')
+    setUser(null)
+    setBlogs([])
+  }
 
   if (user === null) {
     return (
@@ -64,7 +83,8 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p> <i>{ user.name }</i> logged in </p>
+      <p> <i>{ user.name }</i> logged in <button onClick={handleUserLogout}>Logout</button></p>
+      
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
