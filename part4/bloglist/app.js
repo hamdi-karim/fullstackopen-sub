5d6d5aefb60457c0cde642edfee5be5631013039
karim <k.hamdi1995@gmail.com>
2022-11-22 10:21:07 +0100

    backend changes for cypress configuration - ex5.17 step 1

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -30,6 +30,10 @@ app.use(express.json())
 app.use(middleware.tokenExtractor)
 app.use(middleware.requestLogger)
 
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
 app.use('/api/login', loginRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/blogs', middleware.userExtractor,  blogsRouter)
