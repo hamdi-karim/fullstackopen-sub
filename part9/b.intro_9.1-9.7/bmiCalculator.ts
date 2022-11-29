96c15759b63eec8362ce786d907576008d9c2463
karim <k.hamdi1995@gmail.com>
2022-11-29 18:12:47 +0100

    set up ts project and calculateBmi function - ex9.1

---
+++ part9/b.intro_9.1-9.7/bmiCalculator.ts
@@ -0,0 +1,23 @@
+const bmiCalculator = (height: number, weight: number): string => {
+    const heightInMeters: number = height / 100;
+    const bmi = weight / (heightInMeters * heightInMeters);
+    if (bmi >= 18.5 || bmi <= 24.9) {
+        return 'Normal (healthy weight)';
+    } else if (bmi < 18.5) {
+        return 'Underweight (nutrition deficiency)'
+    } else if (bmi > 24.9) {
+        return 'Overweight (health risks)'
+    } else {
+        throw new Error('BMI Calculator Failed!');
+    }
+}
+
+try {
+    console.log(bmiCalculator(185, 80));
+  } catch (error: unknown) {
+    let errorMessage = 'Something went wrong.'
+    if (error instanceof Error) {
+      errorMessage += ' Error: ' + error.message;
+    }
+    console.log(errorMessage);
+}
