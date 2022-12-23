142e440faa691048fc39e21a3b13e207787ed3ea
karim <k.hamdi1995@gmail.com>
2022-12-23 18:29:15 +0100

    move redux logic into Create new Anecdote child compoenent - ste5

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,5 +1,7 @@
 import { useSelector, useDispatch } from 'react-redux'
-import { voteAnecdote, addAnecdote } from "./reducers/anecdoteReducer";
+import { voteAnecdote } from "./reducers/anecdoteReducer";
+
+import AnecdoteForm from "./components/AnecdoteForm";
 
 const App = () => {
   const anecdotes = useSelector(state => state)
@@ -7,20 +9,12 @@ const App = () => {
 
   const sortedAnecdotes = [...anecdotes].sort((a, b) => {
     if (a.votes < b.votes) return 1
-    else return -1
+    else if (a.votes > b.votes) return -1
+    else return 0
   })
 
   const vote = (id) => {
-    //console.log('vote', id)
     dispatch(voteAnecdote(id))
-  }
-
-  const createAnecdote = (e) => {
-    e.preventDefault();
-    const content = e.target.anecdote.value;
-    e.target.anecdote.value = '';
-    dispatch(addAnecdote(content))
-
   }
 
   return (
@@ -37,11 +31,7 @@ const App = () => {
           </div>
         </div>
       )}
-      <h2>create new</h2>
-      <form onSubmit={createAnecdote}>
-        <div><input name='anecdote' /></div>
-        <button>create</button>
-      </form>
+      <AnecdoteForm />
     </div>
   )
 }
---
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -0,0 +1,26 @@
+import { useDispatch } from 'react-redux'
+import {  addAnecdote } from "../reducers/anecdoteReducer";
+
+const AnecdoteForm = () => {
+    const dispatch = useDispatch()
+
+    const createAnecdote = (e) => {
+        e.preventDefault();
+        const content = e.target.anecdote.value;
+        e.target.anecdote.value = '';
+        dispatch(addAnecdote(content))
+    
+      }
+
+  return (
+    <>
+        <h2>create new</h2>
+        <form onSubmit={createAnecdote}>
+            <div><input name='anecdote' /></div>
+            <button>create</button>
+        </form>
+    </>
+  )
+}
+
+export default AnecdoteForm
